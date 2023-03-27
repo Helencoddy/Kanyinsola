8bba20619e3b851048a95bec78f51bdcef00f90f
Kanyinsola Cole <114655270+Helencoddy@users.noreply.github.com>
2023-03-27 11:02:49 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log
+(` 1. My name is Kanyinsola Cole. 
+2. Javascript is my favourite language. 
+3. In the the next one year I see myself getting comfortable building products that positively influence the world and exploring new technologies as the tech ecosystem changes and new technologies emerge.`);
